6c4c01f294ceadffcca14411af5f252a8e285ff9
Himynameislou <louie1598@gmail.com>
2019-07-24 17:01:43 +0000

    Image Blur #1 Solution for Review FINAL

--- blur1.rb
+++ blur1.rb
@@ -9,9 +9,7 @@ class Image
       puts subarray.join 
     end
   end
-
 end
-
 
 image = Image.new([
   [0, 0, 0, 0],
@@ -20,3 +18,8 @@ image = Image.new([
   [0, 0, 0, 0]
 ])
 image.output_image
+
+# Notes for Self
+# When adding .new to object make sure you 'def initialize' in the class for that object.
+# Store your data in a variable so that your 'end' doesn't dump it.
+# Make sure to add .join to make image come out as required by challenge.
