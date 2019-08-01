a782f59f8d6b0e368ab0a4dfcbf22603dac6b9d4
Himynameislou <louie1598@gmail.com>
2019-08-01 23:15:25 +0000

    Blur 2 Final Submission

--- blur2.rb
+++ blur2.rb
@@ -1,32 +1,39 @@
 class Image
+
   def initialize(picture)
     @pic = picture
-    # puts @pic
   end
 
   def output_image
-    @pic.each do |subarray|
-      puts subarray.join 
-    end
-
-    # puts "-----------"
-    # @pic[2][3] = 7
-    # puts @pic[2][3]
+    @pic.each do |row|
+      puts row.join
+    end 
   end
 
-  def blur
-    # look for the 1's
-    ones = []
-    @pic.each_with_index do |row, rowindex|
-      row.each_with_index do |column, columnindex|
-        if column == 1
-          print "rowindex=",rowindex, "     columnindex=",columnindex, "\n"
-          ones<<[rowindex,columnindex]
+  def blurdist(distance=1)
+    distance.times do
+      blur!
+    end
+  end
+
+  private
+
+    def blur!
+      blur = []
+      @pic.each_with_index do |row, i|
+        row.each_with_index do |x, row_i|
+          blur << [i, row_i] if x == 1
         end
       end
-    end
-  end
 
+      blur.each do |coord|
+        @pic[coord[0]][coord[1] + 1] = 1 if coord[1] + 1 <= @pic[coord[0]].length - 1
+        @pic[coord[0]][coord[1] - 1] = 1 if coord[1] - 1 >= 0
+        @pic[coord[0] + 1][coord[1]] = 1 if coord[0] + 1 <= @pic.length - 1
+        @pic[coord[0] - 1][coord[1]] = 1 if coord[0] - 1 >= 0
+      end
+    end
+    
 end
 
 image = Image.new([
@@ -35,7 +42,7 @@ image = Image.new([
   [0, 0, 0, 1],
   [0, 0, 0, 0]
 ])
-image.blur
+image.blurdist
 image.output_image
 
 
@@ -44,3 +51,12 @@ image.output_image
 # When adding .new to object make sure you 'def initialize' in the class for that object.
 # Store your data in a variable so that your 'end' doesn't dump it.
 # Make sure to add .join to make image come out as required by challenge.
+
+
+    # puts "-----------"
+    # @pic[2][3] = 7
+    # puts @pic[2][3]
+
+
+
+# Help from Mentor and youtube/google
